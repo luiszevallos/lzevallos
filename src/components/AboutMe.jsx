68b028fabd51af0166c343940cd1d9b257fd9208
Luis zevallos <luis_zevallos_17@hotmail.com>
2023-06-26 09:17:43 -0400

    ajuste

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import ListAltIcon from '@mui/icons-material/ListAlt';
-import styled from 'styled-components';
-import Photo from '../assets/profile.jpg';
-import TitlePrimary from './TitlePrimary';
-import { StateContext } from '../contexts/useStateContext';
+import React, { useContext } from "react";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import styled from "styled-components";
+import Photo from "../assets/profile.jpg";
+import TitlePrimary from "./TitlePrimary";
+import { StateContext } from "../contexts/useStateContext";
 
 export default function AboutMe() {
   const { state } = useContext(StateContext);
@@ -21,8 +21,14 @@ export default function AboutMe() {
             <P>{aboutMe}</P>
           </div>
           <Button>
-            <A href="download/cv.pdf" download="CV Luis Zevallos">
-              <ListAltIcon style={{marginRight: "5px"}} sx={{ color: "#fff", fontSize: 17 }} />
+            <A
+              href="download/CV_Luis_Zevallos_(Development_Frontend).pdf"
+              download="CV Luis Zevallos (Development Frontend)"
+            >
+              <ListAltIcon
+                style={{ marginRight: "5px" }}
+                sx={{ color: "#fff", fontSize: 17 }}
+              />
               Descargar CV
             </A>
           </Button>
@@ -36,7 +42,7 @@ const DivContain = styled.div`
   max-width: 1024px;
   margin: 20px auto 0;
   width: 100%;
-`
+`;
 const Div = styled.div`
   display: grid;
   grid-gap: 20px;
@@ -45,27 +51,27 @@ const Div = styled.div`
   @media (max-width: 700px) {
     grid-template-columns: 1fr;
   }
-`
+`;
 const DivImage = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 const Img = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 50%;
   box-shadow: 10px 10px 20px #f1f2f3;
-`
+`;
 const DivDetails = styled.div`
   padding: 20px;
   border-radius: 10px;
   box-shadow: 10px 8px 20px #f1f2f3;
-`
+`;
 const P = styled.p`
   color: #6b6b6b;
-  font-size: .9em;
-`
+  font-size: 0.9em;
+`;
 const Button = styled.button`
   border: 0;
   cursor: pointer;
@@ -75,7 +81,7 @@ const Button = styled.button`
   :active {
     transform: scale(0.9);
   }
-`
+`;
 
 const A = styled.a`
   color: #ffffff;
@@ -83,4 +89,4 @@ const A = styled.a`
   font-weight: bold;
   align-items: center;
   text-decoration: none;
-`
+`;
--- src/components/SocialNetwork.jsx
+++ src/components/SocialNetwork.jsx
@@ -1,36 +1,48 @@
-import React from "react"
-import styled from "styled-components"
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import React from "react";
+import styled from "styled-components";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 export default function SocialNetwork() {
   return (
     <DivContain>
       <DivIcon>
-        <a href="https://wa.me/message/SHCANIAWVW6WL1" target="_blank" rel="noreferrer">
-          <WhatsAppIcon sx={{ color: "#ffffff", fontSize: 50 }}/>
+        <a
+          href="https://wa.me/message/SHCANIAWVW6WL1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <WhatsAppIcon sx={{ color: "#ffffff", fontSize: 50 }} />
         </a>
       </DivIcon>
       <DivIcon>
-        <a href="https://www.linkedin.com/in/lzevallos20/" target="_blank" rel="noreferrer">
-          <LinkedInIcon sx={{ color: "#ffffff", fontSize: 50 }}/>
+        <a
+          href="https://www.linkedin.com/in/lzevallos20/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon sx={{ color: "#ffffff", fontSize: 50 }} />
         </a>
       </DivIcon>
       <DivIcon>
-        <a href="https://www.instagram.com/lzevallos20/" target="_blank" rel="noreferrer">
-          <InstagramIcon sx={{ color: "#ffffff", fontSize: 50 }}/>
+        <a
+          href="https://www.instagram.com/lzevallosdev/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <InstagramIcon sx={{ color: "#ffffff", fontSize: 50 }} />
         </a>
       </DivIcon>
     </DivContain>
-  )
+  );
 }
 
 const DivContain = styled.div`
   display: flex;
   margin: 50px 0;
   justify-content: center;
-`
+`;
 
 const DivIcon = styled.div`
   cursor: pointer;
@@ -41,4 +53,4 @@ const DivIcon = styled.div`
   :active {
     transform: scale(0.9);
   }
-`
+`;
